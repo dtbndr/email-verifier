68e0c37c2c17390f4519c3af0bf762b9f77beef2
dtbndr <sharers.contact-0u@icloud.com>
2025-07-10 17:39:52 +0530

    enabled smtp check added a requests.http file

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -13,7 +13,8 @@ import (
 )
 
 func GetEmailVerification(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	verifier := emailVerifier.NewVerifier()
+	verifier := emailVerifier.NewVerifier().
+		EnableSMTPCheck()
 	ret, err := verifier.Verify(ps.ByName("email"))
 	if err != nil {
 		http.Error(w, err.Error(), 500)
